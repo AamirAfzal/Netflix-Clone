a11c70d05af3db99f41ffb11a69568a093d425b1
AamirAfzal <aamirafzal9669@gmail.com>
2025-05-07 16:47:08 +0500

    Video Player

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
-  return <div className="player">Player</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    site: "",
+    type: "",
+    published_at: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDc2ZGRmMjY0MDI0YWI2MTdjOWVmMDBiYTg5NmE4OSIsIm5iZiI6MTc0NjYxMzY5NS4yNDEsInN1YiI6IjY4MWIzNWJmMDkxMDZlZjE4MWVmMGNhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9scGYBSFHxts8tpD-EkFjjVRjwQ2aBX4CHaOCiaSp8M",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        id ? id : "693134"
+      }/videos?language=en-US`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .then(console.log("API DATA", apiData))
+      .catch((err) => console.error(err));
+  }, []);
+  return (
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameborder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
